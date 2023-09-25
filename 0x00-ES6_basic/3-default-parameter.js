e91182f972032923a7392a424f8b291a3a4ca508
nanyamax <ifunanyasunday7@gmail.com>
2023-09-25 23:17:48 +0300

    corrections

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,2 @@
-const getSumOfHoods = (
-  initialNumber = 0,
-  expansion1989 = 89,
-  expansion2019 = 19
-) => initialNumber + expansion1989 + expansion2019;
-
-export default getSumOfHoods;
+export default (initialNumber, expansion1989 = 89, expansion2019 = 19) =>
+  initialNumber + expansion1989 + expansion2019;
