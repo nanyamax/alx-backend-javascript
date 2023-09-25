6112f12faf21953c4967c9fd803bebd049830ecb
nanyamax <ifunanyasunday7@gmail.com>
2023-09-26 02:21:52 +0300

    corrections

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue(array = [''], appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const index = array.indexOf(idx);
     array[index] = `${appendString}${idx}`;
