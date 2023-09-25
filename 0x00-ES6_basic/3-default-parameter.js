0fbb00cb88690f2c126ead3b6fc76d93ebf33bac
nanyamax <ifunanyasunday7@gmail.com>
2023-09-25 22:46:43 +0300

    correction

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,8 +1,15 @@
-export default function getSumOfHoods(
-  initialNumber,
-  expansion1989,
-  expansion2019
-) {
-  if (expansion1989 && expansion2019 !== undefined)
-    return initialNumber + expansion1989 + expansion2019;
-}
+// export default function getSumOfHoods(
+//   initialNumber,
+//   expansion1989 = 89,
+//   expansion2019 = 19
+// ) {
+//   if (expansion1989 && expansion2019 !== undefined)
+//     return initialNumber + expansion1989 + expansion2019;
+// }
+
+const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) =>
+  initialNumber +
+  (expansion1989 ? expansion1989 : 89) +
+  (expansion2019 ? expansion2019 : 19);
+
+export default getSumOfHoods;
