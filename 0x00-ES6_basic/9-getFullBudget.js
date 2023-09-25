0e11e8936625ada52d91aaa4b171465466d8c917
nanyamax <ifunanyasunday7@gmail.com>
2023-09-26 01:37:24 +0300

    corrections

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,7 +1,6 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...getBudgetObject(income, gdp, capita),
     getIncomeInDollars(income) {
