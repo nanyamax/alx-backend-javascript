ac155e6feb79b1fd5fc3de695f319f561959b65b
nanyamax <ifunanyasunday7@gmail.com>
2023-09-25 22:27:57 +0300

    correction

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,7 @@
 export default function getSumOfHoods(
   initialNumber,
-  expansion1989 = 89,
-  expansion2019 = 19
+  expansion1989,
+  expansion2019
 ) {
   if (expansion1989 && expansion2019 !== undefined)
     return initialNumber + expansion1989 + expansion2019;
