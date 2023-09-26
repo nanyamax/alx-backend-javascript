4cdc60dc99e38306cecaedce3438ab5a7ff8bc72
nanyamax <ifunanyasunday7@gmail.com>
2023-09-27 00:12:22 +0300

    boolean parameter to return a promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise(function (resolve, reject) {
+  let promise = new Promise(function (resolve, reject) {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject({ Error: 'The fake API is not working currently' });
     }
   });
+  return promise;
 }
