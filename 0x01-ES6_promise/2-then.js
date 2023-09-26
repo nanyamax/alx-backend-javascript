22ef7c349e893dd06c974928b7242c6927af8f11
nanyamax <ifunanyasunday7@gmail.com>
2023-09-27 02:16:09 +0300

    promise.then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
-    .catch(() => {
-      new Error();
-    })
-    .finally(() => console.log('Got a response from the API'));
+  promise = new Promise((resolve, reject) => {
+    resolve({ status: 200, body: 'Success' });
+    reject(new Error());
+  });
+  promise.then(function Success() {
+    console.log('Got a response from the API');
+  });
 }
