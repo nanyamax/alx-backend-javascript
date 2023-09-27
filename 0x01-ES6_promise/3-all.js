7888b6152a7c7fb6d9ff93a7f5502e3e84304615
nanyamax <ifunanyasunday7@gmail.com>
2023-09-27 05:43:30 +0300

    promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
       console.log(
         `${response[0].body} ${response[1].firstName} ${response[1].lastName}`
