012f123155b99464ff88783ce65ca7ab31d3beb9
nanyamax <ifunanyasunday7@gmail.com>
2023-09-27 06:25:07 +0300

    promise.all

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const tmp = [];
+  const tmp1 = signUpUser(firstName, lastName)
+    .then((response) => tmp.push({ status: 'success', value: response }))
+    .catch((error) => tmp.push({ status: 'error', value: error }));
+
+  const tmp2 = uploadPhoto(fileName)
+    .then((response) => tmp.push({ status: 'success', value: response }))
+    .catch((error) => tmp.push({ status: 'error', value: error }));
+
+  return Promise.all([tmp1, tmp2]).finally(() => tmp);
 }
