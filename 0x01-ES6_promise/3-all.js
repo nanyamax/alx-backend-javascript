980a63a661269e2d9532d107a05749b5a2e5b7ff
nanyamax <ifunanyasunday7@gmail.com>
2023-09-27 05:35:25 +0300

    promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((data) =>
-      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`)
+  const tmp1 = uploadPhoto();
+  const tmp2 = createUser();
+  Promise.all([tmp1, tmp2])
+    .then((response) =>
+      console.log(
+        `${response[0].body} ${response[1].firstName} ${response[1].lastName}`
+      )
     )
     .catch(() => console.log('Signup system offline'));
 }
