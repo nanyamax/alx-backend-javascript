6560f7be956b409f67166be34759a2c4629a163f
nanyamax <ifunanyasunday7@gmail.com>
2023-09-29 02:30:58 +0300

    implement a class that extends

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,15 +1,19 @@
 import Building from './5-building';
+
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this.floors = floors;
   }
+
   get floors() {
     return this._floors;
   }
+
   set floors(floor) {
     this._floors = floor;
   }
+
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`;
   }
