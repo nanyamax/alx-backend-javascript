1f712cb39959232814868522eac7801504ab8128
nanyamax <ifunanyasunday7@gmail.com>
2023-09-28 22:00:42 +0300

    implement a method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+  // getter for code
+  get code() {
+    return this._code;
+  }
+  // setter for code
+  set code(symbol) {
+    this._code = symbol;
+  }
+  // getter for currency name
+  get name() {
+    return this._name;
+  }
+  // setter for currency name
+  set name(currencyName) {
+    this._name = currencyName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name}(${this._code})`;
+  }
+}
