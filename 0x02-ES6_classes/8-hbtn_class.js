4840a3628c0628b1727a8e6a89031f09421b0ec5
nanyamax <ifunanyasunday7@gmail.com>
2023-09-29 01:38:53 +0300

    to primitive

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -19,6 +19,7 @@ export default class HolbertonClass {
   set location(coordinate) {
     this._location = coordinate;
   }
+
   [Symbol.toPrimitive](cast) {
     if (cast === 'number') {
       return this.size;
