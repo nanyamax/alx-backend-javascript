d492535603772630f18d5471244aabb451686743
nanyamax <ifunanyasunday7@gmail.com>
2023-09-28 19:01:54 +0300

    implement getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  //getter for name attribute
+  get name() {
+    return this._name;
+  }
+
+  // setter for name attribute
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  //getter for length
+  get length() {
+    return this._length;
+  }
+
+  // setter for length
+  set length(Length) {
+    if (typeof Length === 'number') {
+      this._length = Length;
+    } else {
+      throw TypeError('Length must be a number');
+    }
+  }
+
+  // getter for holbertCourse.student
+  get students() {
+    return this._students;
+  }
+
+  // setter for holbertCourse.student
+  set students(student) {
+    if (Array.isArray(student)) this._students = student;
+  }
+}
