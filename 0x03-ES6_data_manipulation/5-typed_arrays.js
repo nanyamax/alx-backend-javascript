7dcb9aae8df094e6703cbf11ebe23f6d2d85bc59
nanyamax <ifunanyasunday7@gmail.com>
2023-10-04 00:24:09 +0300

    a function that returns a new ArrayBuffer with an int8 value

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+  const int8 = new Int8Array(length);
+  int8[position] = value;
+
+  const { buffer } = int8;
+  const view = new DataView(buffer, 0, length);
+  return view;
+}
