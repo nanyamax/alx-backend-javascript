6916cb2727c35bb147fd2fcbaa8211dffb732ee7
nanyamax <ifunanyasunday7@gmail.com>
2023-10-03 17:25:59 +0300

    using chaining

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+export default function updateStudentGradeByCity(
+  students,
+  studentsCity,
+  newGrades
+) {
+  const getStudentGrade = (student) => {
+    return newGrades.find((grade) => grade.studentId === student.id);
+  };
+
+  const locatedStudents = students.filter(
+    (student) => student.location === studentsCity
+  );
+
+  return locatedStudents.map((student) => {
+    const foundStudent = getStudentGrade(student);
+    return { ...student, grade: foundStudent ? foundStudent.grade : 'N/A' };
+  });
+}
