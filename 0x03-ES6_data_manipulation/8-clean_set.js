7e5c117cfd4f1f0a68c27ce903b6fd07b8290a08
nanyamax <ifunanyasunday7@gmail.com>
2023-10-04 02:27:53 +0300

    a function that returns the rest of a string when the value starts with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (!startString || startString.length < 0) return '';
+  let string = '';
+  for (const element of set) {
+    if (element && element.startsWith(startString)) {
+      string +=
+        string.length === 0
+          ? element.replace(startString, '')
+          : element.replace(startString, '-');
+    }
+  }
+  return string;
+}
